895bb426062ba9831c0f36fe9abf8afd41419fd5
mbkarle <mkarle2178@gmail.com>
2017-06-24 18:47:24 -0400

    new animations and mechanics

--- dungeonAdventure.js
+++ dungeonAdventure.js
@@ -4,6 +4,11 @@ function Character(name, strength, dexterity, vitality) {
   this.dexterity = dexterity;
   this.vitality = vitality;
 }
+function Item(name, damage, vitality){
+  this.name = name;
+  this.damage = damage;
+  this.vitality = vitality;
+}
 function Dex(Character){
   return Math.pow(Math.random(), 1 / (Character.dexterity / 3));
 }
@@ -12,28 +17,58 @@ function Damage(Character, target){
   target.vitality -= hit;
   document.getElementById("hero").innerHTML = Hero.vitality;
   document.getElementById("enemy").innerHTML = Troglodyte.vitality;
+  document.getElementById("defend").innerHTML = "Shield: " + HeroShield.vitality;
   console.log(target.name + " was hurt");
 }
-
+function Shield(){
+  Hero.vitality += 2;
+  protected = true;
+}
 var Hero = new Character("The Hero", 5, 2, 20);
 var Troglodyte = new Character("the Troglodyte", 3, 1, 50);
+var HeroShield = new Item("the shield", null, 20);
+var protected = false;
+var shielded;
 function myGame(){
   window.onload = function(){
     document.getElementById("hero").innerHTML = Hero.vitality;
     document.getElementById("enemy").innerHTML = Troglodyte.vitality;
-    document.getElementById("attack").onclick = function() {Damage(Hero, Troglodyte)};
-    //if(Troglodyte.vitality > 0){
-    var enemyAttack = setInterval(function() {Damage(Troglodyte, Hero);}, 4000);
+    document.getElementById("defend").innerHTML = "Shield: " + HeroShield.vitality;
+    document.getElementById("attack").onclick = function() {
+      Damage(Hero, Troglodyte);
+//jquery animations:
+      $(".attackCooldown").show();
+      $(".attackCooldown").animate({
+        width: '0px'
+      }, 10000, function() {
+        $(".attackCooldown").hide();
+      $(".attackCooldown").animate({
+        width: '87px'
+      }, 1);
+    });
+    };
+    if(protected == false && HeroShield.vitality > 0){
+    document.getElementById("defend").onclick = function() {
+      shielded = setInterval(function() {Shield()}, 4000);
 
-    //TODO: onload only happens once! check this every click?
-    // window.clearInterval(enemyAttack);
-    // console.log("should be cleared...");
+    }}
+    var enemyAttack = setInterval(function() {if(protected == true){Damage(Troglodyte, HeroShield)} else{Damage(Troglodyte, Hero)}}, 4000);
     window.onclick = function(){
+      console.log("clicked");
+      if(protected == true || HeroShield.vitality <= 0){
+        window.clearInterval(shielded);
+        protected = false;
+        //jquery animation:
+          $(".defendCooldown").hide('fast');
+      }
       if(Troglodyte.vitality < 0){
         window.clearInterval(enemyAttack);
-        console.log("interval cleared!");
-      }
+      };
     };
+    //jquery animation:
+    $("#defend").click(function(){
+      $(".defendCooldown").toggle(4000);
+      })
   };
 
 }
