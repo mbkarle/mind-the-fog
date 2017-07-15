1cee6586494a655b2d7802ac117ae3f2add22fbc
Alexander Karle <akarle@umass.edu>
2017-07-15 15:06:44 -0400

    fixed typo

--- dungeonAdventure.js
+++ dungeonAdventure.js
@@ -1,4 +1,4 @@
-world_height//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
 // This script should be the game runner. Other classes should live in the
 // ./world_objects/ folder,although this script can have helper methods and global variables.
 //------------------------------------------------------------------------------------------------
