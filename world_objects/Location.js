ff4491530fc7dc7ed36884f96e5210fd98965572
Alexander Karle <akarle@umass.edu>
2017-07-16 18:34:40 -0400

    fixed #17 to clean up location OOP

--- world_objects/Location.js
+++ world_objects/Location.js
@@ -1,10 +1,9 @@
 class Location {
-    constructor(rowID, colID){
-        //DECIDED ALL THESE SHOULD BE FILE SPECIFIC!
-        // this.name = name; //name of the Location
-        // this.message = message; //message displayed on print()
-        // this.objid = objid; //object id
-        // this.symbol = symbol; //symbol to display on map
+    constructor(rowID, colID, name, objid, symbol, message){
+        this.name = name; //name of the Location
+        this.message = message; //message displayed on print()
+        this.objid = objid; //object id
+        this.symbol = symbol; //symbol to display on map
         this.hero_present = false; //whether or not the hero is on this Location
         this.xCoord = colID * 15; //pixel coords
         this.yCoord = rowID * 15;
@@ -16,11 +15,7 @@ class Location {
 
 class Chest extends Location {
     constructor(rowID, colID){
-        super(rowID, colID);
-        this.name = 'Treasure Chest'; //name of the Location
-        this.message = "A wooden chest. It's locked, but no wood can withstand your blade."; //message displayed on print()
-        this.objid = 'treasure'; //object id
-        this.symbol = 'v'; //symbol to display on map
+        super(rowID, colID,'Treasure Chest', 'treasure', 'v', "A wooden chest. It's locked, but no wood can withstand your blade.");
         this.emptied_chest = false; //has the chest been emptied?
         this.treasureID = -1; //id of treasure Item inside in itemList
     }
@@ -28,20 +23,12 @@ class Chest extends Location {
 
 class Trapdoor extends Location {
     constructor(rowID, colID){
-        super(rowID, colID);
-        this.name = 'Trapdoor';
-        this.message = 'A gaping black hole stares at you from the floor of the dungeon... you wonder what is on the other side';
-        this.objid = 'trapdoor'
-        this.symbol = 'ø'
+        super(rowID, colID, 'Trapdoor', 'trapdoor','ø',  'A gaping black hole stares at you from the floor of the dungeon... you wonder what is on the other side');
     }
 }
 
 class Tile extends Location {
     constructor(rowID, colID){
-        super(rowID, colID)
-        this.name = "Tile"
-        this.message = ""
-        this.objid = "tile"
-        this.symbol = '.' //style preference? '·' instead??
+        super(rowID, colID, 'Tile', "tile", '.', '')//style preference? '·' instead??
     }
 }
