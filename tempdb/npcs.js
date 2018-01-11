229cb6f06d305294c28624c3ff978b36d3d320fb
Alexander Karle <akarle@umass.edu>
2018-01-10 20:38:41 -0500

    Changing JSON from ' to " quotes

--- tempdb/npcs.js
+++ tempdb/npcs.js
@@ -4,10 +4,10 @@ var NPCS = {
         "charDisplay": "the alchemist",
         "active": false,
         "roomIdx": 1,
-        'coords': [3, 15],
-        'symbol': 'A',
-        'description': 'A pungent smell wafts towards you from where the alchemist sits, peddling his wares.',
-        'merchandise': [],
+        "coords": [3, 15],
+        "symbol": "A",
+        "description": "A pungent smell wafts towards you from where the alchemist sits, peddling his wares.",
+        "merchandise": [],
         "buyFunc": function(id, buyerInv, sellerInv, frac) {
             // Try to buy and transfer (but keepOrig=true)
             if(sellerInv.transfer_for_gold(buyerInv, id, undefined, frac, true)){
@@ -16,8 +16,8 @@ var NPCS = {
                 var item = sellerInv.get(id)
                 if(!item.prototyped){
                     item.prototyped = true;
-                    for(var i = 0; i < CONSUMABLES[item.name]['itemlists'].length; i++){
-                        itemListMeta[CONSUMABLES[item.name]['itemlists'][i]].push(item);
+                    for(var i = 0; i < CONSUMABLES[item.name]["itemlists"].length; i++){
+                        itemListMeta[CONSUMABLES[item.name]["itemlists"][i]].push(item);
                     }
                     refillChests();
                 }
@@ -32,10 +32,10 @@ var NPCS = {
         "charDisplay": "the shield maker",
         "active": false,
         "roomIdx": 1,
-        'coords': [3, 6],
-        'symbol': 'S',
-        'description': 'The shield maker can improve your shield, for a price.',
-        'merchandise': [],
+        "coords": [3, 6],
+        "symbol": "S",
+        "description": "The shield maker can improve your shield, for a price.",
+        "merchandise": [],
         "buyFunc": function(id, buyerInv, sellerInv, frac) {
             var item = sellerInv.get(id)
 
@@ -69,19 +69,19 @@ var NPCS = {
         "active": false,
         "roomIdx": 1,
         "coords": [9, 15],
-        'symbol': 'T',
-        'description': "The dog's plenty loyal. The trainer will put it to work.",
-        'merchandise': []
+        "symbol": "T",
+        "description": "The dog's plenty loyal. The trainer will put it to work.",
+        "merchandise": []
     },
 
     "blacksmith":{
-        "charID": 'blacksmith',
-        'charDisplay': 'the blacksmith',
-        'roomIdx': 1,
-        'coords': [9, 25],
-        'symbol': 'B',
-        'description': "The blacksmith will fill the dungeon with his finest crafts.",
-        'merchandise': [],
+        "charID": "blacksmith",
+        "charDisplay": "the blacksmith",
+        "roomIdx": 1,
+        "coords": [9, 25],
+        "symbol": "B",
+        "description": "The blacksmith will fill the dungeon with his finest crafts.",
+        "merchandise": [],
         "buyFunc": function(id, buyerInv, sellerInv, frac) {
             var item = sellerInv.get(id)
 
@@ -106,13 +106,13 @@ var NPCS = {
     },
 
     "winston":{
-        "charID": 'winston',
-        'charDisplay': "Winston",
-        'active': false,
-        'roomIdx': 1,
-        'coords': [3, 25],
-        'symbol': 'W',
-        'description': 'Winston will lend you his powers, for a price.',
-        'merchandise': []
+        "charID": "winston",
+        "charDisplay": "Winston",
+        "active": false,
+        "roomIdx": 1,
+        "coords": [3, 25],
+        "symbol": "W",
+        "description": "Winston will lend you his powers, for a price.",
+        "merchandise": []
     }
 }
