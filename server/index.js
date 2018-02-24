4098499c2f26a845eb524b9f16594d99adeb498d
Alexander Karle <akarle@umass.edu>
2018-02-24 11:19:28 -0500

    Adding basic home page for game

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.get('/', (req, res) => res.redirect('/index.html'))
+app.get('/', (req, res) => res.redirect('/home.html'))
 
 app.use(express.static(path.join(__dirname, '..', 'game')))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
